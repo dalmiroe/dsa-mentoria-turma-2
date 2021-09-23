4827a9ab26afa76022661a5ccb14e37fa73394cc
Dalmiro <dalmiroe@gmail.com>
2021-09-22 21:52:34 -0300

    Inclusão de novas imagens e rostos

--- js/apoio.js
+++ js/apoio.js
@@ -1,22 +1,27 @@
 //erroMessage = getElement('.error');
 var suggeri;
 var divReco = document.querySelector('.divRecomendacaox');
-var p1 = document.getElementById("p1");
-var v1 = document.getElementById("v1");
-var nr ;
 
-var listaImagens = ["image/product-1.png", "image/product-2.png",
-"image/product-3.png", "image/product-4.png","image/product-5.png",
-"image/product-6.png", "image/product-7.png","image/product-8.png"
-];
-var listaValores = ["R$971,00","R$502", "R$58","R$888","R$702", "R$158","R$742", "R$202,20",
+
+var listaImagens = ["image/product-1.png","image/product-2.png","image/product-3.png", "image/product-4.png",
+                    "image/product-5.png","image/product-6.png", "image/product-7.png","image/product-8.png",
+                    "image/product-9.png","image/product-10.png","image/product-11.png","image/product-12.png",
+                    "image/product-13.png","image/product-14.png","image/product-15.png","image/product-16.png"
+                  ];
+
+var listaValores = ["R$971,00","R$502", "R$58","R$888",
+                    "R$702", "R$158","R$742","R$202,20",
+                    "R$602,00","R$100,00","R$200,00","200,00",
+                    "R$45,00","R$25,00","R$15,00","78,00"
 ];
 
 const urlSuggeri = 'https://cd7jwv2auk4wzkrn.anvil.app/_/private_api/NAUJWAV2RPBGYXSGAHBTFQXI/predict_item/fca413d5/A39HTATAQ9V7YF';
 
 function getNr() {
-  return Math.random();
+  return  Math.floor(Math.random() *  16);
 }
+
+
 
 function MontaListaProduto () {
   lista = `
@@ -29,24 +34,24 @@ function MontaListaProduto () {
     </div>`;
 
     document.getElementById("p1").innerText = "Produto   " + suggeri[0];
-    document.getElementById("v1").innerText = listaValores[7];
-    document.getElementById("i1").src = listaImagens[7];
+    document.getElementById("v1").innerText = listaValores[getNr()];
+    document.getElementById("i1").src = listaImagens[getNr()];
 
     document.getElementById("p2").innerText = "Produto   " + suggeri[1];
-    document.getElementById("v2").innerText = listaValores[6];
-    document.getElementById("i2").src = listaImagens[6];
+    document.getElementById("v2").innerText = listaValores[getNr()];
+    document.getElementById("i2").src = listaImagens[getNr()];
 
     document.getElementById("p3").innerText = "Produto   " + suggeri[2];
-    document.getElementById("v3").innerText = listaValores[5];
-    document.getElementById("i3").src = listaImagens[5];
+    document.getElementById("v3").innerText = listaValores[getNr()];
+    document.getElementById("i3").src = listaImagens[getNr()];
 
     document.getElementById("p4").innerText = "Produto   " + suggeri[3];
-    document.getElementById("v4").innerText = listaValores[4];
-    document.getElementById("i4").src = listaImagens[4];
+    document.getElementById("v4").innerText = listaValores[getNr()];
+    document.getElementById("i4").src = listaImagens[getNr()];
 
     document.getElementById("p5").innerText = "Produto   " + suggeri[4];
-    document.getElementById("v5").innerText = listaValores[3];
-    document.getElementById("i5").src = listaImagens[3];
+    document.getElementById("v5").innerText = listaValores[getNr()];
+    document.getElementById("i5").src = listaImagens[getNr()];
 
   return lista;
 }
